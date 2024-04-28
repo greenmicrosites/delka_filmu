8f15f56fe474b7d45d378f3ef42f66a68f02fb6c
Petra Vicherkova <peta.vicherkova@gmail.com>
2024-04-28 11:22:32 +0200

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+let length = 223
+let hodiny = 223 / 60
+let minuty = 223 % 60
+
+document.querySelector(".hours").innerHTML = Math.floor(hodiny)
+document.querySelector(".minutes").innerHTML = minuty
